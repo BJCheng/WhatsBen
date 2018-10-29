8d3f6c2434fd7cff44d62e9b839bd7f340e04f3d
BJCheng <BJCheng@users.noreply.github.com>
2018-10-28 21:33:35 -0400

    adding redis client interface

--- src/api-server.js
+++ src/api-server.js
@@ -1,10 +1,24 @@
 import express from 'express';
+import { setupRedis, redisClient } from './redis-client';
 
 const app = express();
 const port = process.env.PORT || 4000;
 
+setupRedis();
+
 app.get('/', (req, res) => {
 	res.send('Hello whatsapp clone!');
+});
+
+app.get('/redis-test', (req, res) => {
+	redisClient.incr('inc-foo', (err, result) => {
+		if (err) {
+			console.error(err);
+			res.status(500).send('redis increment error');
+		} else {
+			res.send(`New increment value is ${result}`);
+		}
+	});
 });
 
 app.listen(port, () => {
---
+++ src/redis-client.js
@@ -0,0 +1,22 @@
+import redis from 'redis';
+
+export let redisClient = null;
+
+export const setupRedis = () => {
+	if (process.env.REDISCLOUD_URL) {
+		console.log('connecting to redis cloud');
+		redisClient = redis.createClient(process.env.REDISCLOUD_URL);
+	}
+	else {
+		console.log('connecting to local redis');
+		redisClient = redis.createClient();
+	}
+
+	redisClient.on('connect', function () {
+		console.log('redis redisClient emitted connect event');
+	});
+
+	redisClient.on('error', function (err) {
+		console.error(err, 'redis error');
+	});
+};
