aea955bd91e91f256a73c3d2cc72ebba77ba69cc
BJCheng <BJCheng@users.noreply.github.com>
2018-11-10 12:05:04 -0500

    rename to header, body and footer

--- src/view/components/App.jsx
+++ src/view/components/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import Recipient from './recipient/recipient.jsx';
-import MessageList from './message-list/message-list.jsx';
-import InputContainer from './input/input-container.jsx';
+import Recipient from './header/recipient.jsx';
+import MessageList from './body/message-list.jsx';
+import InputContainer from './footer/input-container.jsx';
 // import './normalize.scss';
 
 export default () => {
