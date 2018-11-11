4e6c3d02f2c238017ee47a4ec0171a1bd57a1e44
BJCheng <BJCheng@users.noreply.github.com>
2018-11-10 21:21:32 -0500

    move app.scss

--- src/view/components/App.jsx
+++ src/view/components/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import HeaderContainer from './header/header-container.jsx';
 import BodyContainer from './body/body-container.jsx';
 import FooterContainer from './footer/footer-container.jsx';
-import './app.scss';
+import './styles/app.scss';
 
 export default () => {
   return (
