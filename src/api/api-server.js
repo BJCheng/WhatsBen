661834cbb3c30856280bd8b80c99109a8a8af1e3
BJCheng <BJCheng@users.noreply.github.com>
2018-11-15 17:14:00 -0500

    setup socket server when initialize server

--- src/api/api-server.js
+++ src/api/api-server.js
@@ -4,6 +4,7 @@ import { setupRedis } from './redis-client';
 import setupRoutes from './routes';
 import cors from 'cors';
 import bodyParser from 'body-parser';
+import setupSocketServer from './setup-socket-server';
 
 const port = process.env.PORT || 4000;
 const app = express();
@@ -11,8 +12,9 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 setupRedis();
-setupRoutes(app);
 
-app.listen(port, () => {
+const httpServer = app.listen(port, () => {
   console.log(`Express app listening at port ${port}`);
 });
+setupSocketServer(httpServer);
+setupRoutes(app);
