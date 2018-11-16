7ebbc8cd4fadf72ca27c5fb6d0b5b1fa01bd5773
BJCheng <BJCheng@users.noreply.github.com>
2018-11-15 22:32:24 -0500

    setup client socket

--- src/view/utils/setup-socket.js
+++ src/view/utils/setup-socket.js
@@ -3,11 +3,8 @@ import io from 'socket.io-client';
 export default () => {
   const userSocket = io(`${global.__apiUrl__}/ben`);
   userSocket.emit('send-message', 'hi');
-  userSocket.on('connection', (socket) => {
-    console.log('client socket connected');
-  });
   userSocket.on('receive-message', (msg) => {
-    // dispatch actions
-    console.log(`client receive message back from server: ${msg}`);
+    // TODO: dispatch actions
+    console.log(`client receive message from server: ${msg}`);
   });
 };
