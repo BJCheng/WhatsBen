f71ed782f7ba0fcd3dcf84413ec7ddd751325de6
BJCheng <BJCheng@users.noreply.github.com>
2018-11-15 22:31:59 -0500

    rename action type

--- src/view/reducers/body-reducer.js
+++ src/view/reducers/body-reducer.js
@@ -1,11 +1,11 @@
-import { SEND_MESSAGE } from '../actions/types';
+import { APPEND_MESSAGE } from '../actions/types';
 
 const messageBodyInitialState = {
-  messages: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10','1', '2','3','4']
+  messages: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '1', '2', '3', '4']
 };
 export default (state = messageBodyInitialState, action) => {
   switch (action.type) {
-    case SEND_MESSAGE: {
+    case APPEND_MESSAGE: {
       return { ...state, messages: state.messages.concat(action.value) };
     }
     default:
