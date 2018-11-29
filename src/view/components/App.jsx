7a781bd07e59d55a63bd686fbddde53046ab11ca
BJCheng <BJCheng@users.noreply.github.com>
2018-11-29 13:13:29 +0800

    load to user when before component render

--- src/view/components/App.jsx
+++ src/view/components/App.jsx
@@ -2,14 +2,67 @@ import React from 'react';
 import HeaderContainer from './header/header-container.jsx';
 import BodyContainer from './body/body-container.jsx';
 import FooterContainer from './footer/footer-container.jsx';
+import { connect } from 'react-redux';
 import './styles/app.scss';
+import LocalStorage from '../utils/local-storage';
+import { getUser, redirectToLogin } from '../actions';
 
-export default () => {
-  return (
-    <div className='app'>
-      <HeaderContainer />
-      <BodyContainer />
-      <FooterContainer />
-    </div>
-  );
-};
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.setFromUser = this.setFromUser.bind(this);
+    this.renderModal = this.renderModal.bind(this);
+  }
+
+  componentWillMount() {
+    const { toName } = this.props.match.params;
+    this.props.loadUsers(toName);
+  }
+
+  componentDidMount() {
+    alert(this.props.from);
+  }
+
+  render() {
+    return (
+      <div className='app'>
+        <HeaderContainer />
+        <BodyContainer />
+        <FooterContainer />
+        {this.renderModal()}
+      </div>
+    );
+  }
+
+  setFromUser() {
+  }
+
+  renderModal() {
+    if (this.props.from)
+      return;
+    return (
+      <div>modal</div>
+    );
+  }
+}
+
+const mapStateToProps = (state) => ({
+  from: state.from
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  loadUsers: (toName) => {
+    dispatch(getUser(toName)).then((res) => {
+      if (res.error && res.error.length > 0) {
+        dispatch(redirectToLogin());
+        throw new Error('eeee');
+      }
+    }).then(() => {
+      alert('after catch');
+    });
+  }
+});
+
+export default connect(
+  mapStateToProps, mapDispatchToProps
+)(App);
