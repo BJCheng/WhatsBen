2c1407bd29ecc8f30a7b6068e9c21cbf6140c54e
BJCheng <BJCheng@users.noreply.github.com>
2018-11-29 13:12:33 +0800

    new actions

--- src/view/actions/index.js
+++ src/view/actions/index.js
@@ -1,5 +1,7 @@
-import { CHANGE_INPUT, APPEND_MESSAGE, CLEAR_INPUT } from './types';
+import { CHANGE_INPUT, APPEND_MESSAGE, CLEAR_INPUT, REDIRECT_TO_LOGIN } from './types';
 import axios from 'axios';
+
+const apiUrl = global.__apiUrl__;
 
 export const changeInput = (text) => ({
   type: CHANGE_INPUT,
@@ -12,9 +14,21 @@ export const appendMessageToList = (text) => ({
 });
 
 export const sendMessageAsync = (text) => () => {
-  return axios.post(`${global.__apiUrl__}/send-message`, { msg: text });
+  return axios.post(`${apiUrl}/send-message`, { msg: text });
 };
 
 export const clearInput = () => ({
   type: CLEAR_INPUT
+});
+
+export const loadMessages = (dispatch, getState) => {
+  return axios.get(`${apiUrl}`);
+};
+
+export const getUser = (name) => (dispatch, getState) => {
+  return axios.get(`${apiUrl}/user/${name}`).then(({ data }) => data);
+};
+
+export const redirectToLogin = () => ({
+  type: REDIRECT_TO_LOGIN
 });
--- src/view/actions/types.js
+++ src/view/actions/types.js
@@ -2,3 +2,6 @@ export const CHANGE_INPUT = 'CHANGE_INPUT';
 export const APPEND_MESSAGE = 'APPEND_MESSAGE';
 export const CLEAR_INPUT = 'CLEAR_INPUT';
 export const SET_NAME = 'SET_NAME';
+export const SET_FROM_USER = 'SET_FROM_USER';
+export const SET_TO_USER = 'SET_TO_USER';
+export const REDIRECT_TO_LOGIN = 'REDIRECT_TO_LOGIN';
