ebd0bea18229e95d416bb418135b2060d7a17227
BJCheng <BJCheng@users.noreply.github.com>
2018-11-29 12:24:22 +0800

    others

--- src/view/components/body/body-container.jsx
+++ src/view/components/body/body-container.jsx
@@ -1,15 +1,20 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import MessageList from './message-list.jsx';
+import { } from '../../actions';
 
 class BodyContainer extends Component {
   constructor(props) {
     super(props);
   }
 
+  componentDidMount() {
+    // this.props.dispatch();
+  }
+
   render() {
     return (
-      <div className='body-container' ref={this.bodyContainerRef}>
+      <div className='body-container'>
         <MessageList messages={this.props.messages} />
       </div>
     );
