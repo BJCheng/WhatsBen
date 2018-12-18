5c9e38b3d4ca11ffca64dab63344790983cd38dd
BJCheng <BJCheng@users.noreply.github.com>
2018-12-17 20:49:06 -0500

    RESTful convension refactor

--- src/view/actions/api.js
+++ src/view/actions/api.js
@@ -3,10 +3,3 @@ import axios from 'axios';
 export default axios.create({
   baseURL: global.__apiUrl__
 });
-
-const a = {
-  getUserById: (id) => (baseUrl.get(`/user/${id}`)),
-  fetchMessgeasBetween: (from, to) => baseUrl.get(`/messages/${from}/${to}`),
-  sendMessages: (from, to, text, sendTime) => baseUrl.post('/messages', { from, to, text, sendTime }),
-  setupNamespace: () => baseUrl.put('/setup-namespace')
-};
