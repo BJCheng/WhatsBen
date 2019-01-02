286b51fa5d39ad46ce467ff19dbbf9bcf80ce44c
BJCheng <BJCheng@users.noreply.github.com>
2019-01-01 22:57:51 -0500

    update the order of contact when receiving messages

--- src/view/reducers/contacts-reducer.js
+++ src/view/reducers/contacts-reducer.js
@@ -1,15 +1,14 @@
 import { FETCH_CONTACTS, UPDATE_CONTACT } from '../actions/types';
 
 export default (state = [], action) => {
+  const incomingContact = state.find(contact => contact.id === action.id);
   switch (action.type) {
     case FETCH_CONTACTS:
       return action.contacts;
     case UPDATE_CONTACT:
-      return [action.id].concat(state.map(id => {
-        if (id === action.id)
-          return;
-        else
-          return id;
+      return [incomingContact].concat(state.filter(contact => contact.id !== action.id).map(contact => {
+        if (contact.id !== action.id)
+          return contact;
       }));
     default:
       return state;
